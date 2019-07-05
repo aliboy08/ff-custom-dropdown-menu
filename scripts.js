96810374da56d323f6231f1fd49c529eaa158225
Alistair Ponce <alistair.ponce@gmail.com>
2019-07-05 10:19:48 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+if ($('.burger-menu-trigger').length) {
+	$('body').on('click', '.burger-menu-trigger', function () {
+		var button = $(this);
+		var menu = $(this).next('.menu-container');
+		button.toggleClass('active');
+		menu.toggleClass('active');
+	});
+
+	$('body').click(function (e) {
+		if ($(e.target).closest('#ff-custom-dropdown-menu').length == 0) {
+			custom_dropdown_menu_close();
+		}
+	})
+}
+
+function custom_dropdown_menu_close() {
+	$('#ff-custom-dropdown-menu .burger-menu-trigger').removeClass('active');
+	$('#ff-custom-dropdown-menu .menu-container').removeClass('active');
+}
